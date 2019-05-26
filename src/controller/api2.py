e83f11381f1b3505f98852eee1ed903e4ba4e558
LucianoLima <yoluciano.fernando@gmail.com>
2019-05-26 17:39:31 -0300

    start

---
+++ src/controller/api2.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+from src.model.outra import Outra
+
+#from .model.pessoa import Pessoa
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route("/paginaHTML2")
+def paginaHTML():
+    url="http://journals.ecs.soton.ac.uk/java/tutorial/networking/urls/readingWriting.html"
+    requisicao = requests.get(url)
+    print("Código HTTP de resposta: "+ str(requisicao.status_code))
+    pagina = requisicao.text
+    return pagina
+
+@app.route("/paginaTXT2")
+def paginaTXT():
+    url="http://journals.ecs.soton.ac.uk/java/tutorial/networking/urls/readingWriting.html"
+    requisicao = requests.get(url)
+    print("Código HTTP de resposta: " + str(requisicao.status_code))
+    pagina = requisicao.text
+    soup = BeautifulSoup(pagina)
+    txt = soup.get_text()
+    return txt
+
+
+@app.route("/coleta042", methods=['GET', 'POST'])
+def iniciar_GET_POST():
+    return jsonify({"resp": "Coletor iniciado através do método GET ou POST."})
+
+@app.route("/coleta052", methods=['GET', 'POST'])
+def classopa():
+    outra = Outra(132)
+    pessoa = outra.imprime()
+    return jsonify({"idade": pessoa.getIdade()})
+
+if __name__ == "__main__":
+    app.run()
