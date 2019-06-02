bd16ba39058b0d01af5fbd5a2303b3fa8a0a9c72
LucianoLima <yoluciano.fernando@gmail.com>
2019-06-02 17:08:14 -0300

    Controller Usuario

--- src/controller/UserController.py
+++ src/controller/UserController.py
@@ -6,42 +6,12 @@ from src.service.UserService import UserService
 user_controller = Blueprint('user_controller', __name__, template_folder='templates')
 service = UserService()
 
-@user_controller.route('/user')
-def list():
-    try:
-        objs = service.listAll()
-        list = []
-        for obj in objs:
-            list.append(obj.userToJson())
-        return jsonify(list)
-    except Exception:
-        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
 
-@user_controller.route('/user/<id>')
-def findById(id):
-    try:
-        obj = service.findById(id)
-        return jsonify(obj.userToJson())
-    except:
-        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
 
-@user_controller.route('/user', methods=["POST"])
-def post():
-    if request.get_json() is None:
-        abort(http.HTTPStatus.PRECONDITION_REQUIRED)
-    try:
-        body = request.get_json()
-        obj = Users()
-        obj.dictToUser(body)
-        service.save(obj)
-        return jsonify(obj.userToJson())
-    except Exception:
-        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
-
-@user_controller.route('/user', methods=["PUT"])
+@user_controller.route('/usuario', methods=["PUT"])
 def put():
     if request.get_json() is None:
-        abort(http.HTTPStatus.PRECONDITION_REQUIRED)
+        abort(http.HTTPStatus.BAD_REQUEST)
     try:
         body = request.get_json()
         obj = Users()
@@ -51,13 +21,147 @@ def put():
     except Exception:
         abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
 
-@user_controller.route('/user/<id>', methods=["DELETE"])
+@user_controller.route('/usuario/remove/<id>', methods=["DELETE"])
 def delete(id):
     if id == None:
-        abort(http.HTTPStatus.PRECONDITION_REQUIRED)
+        abort(http.HTTPStatus.PRECONDITION_FAILED)
     try:
         obj = service.findById(id)
+        if(obj.id is None):
+            raise ModuleNotFoundError('Não encontrado')
         service.remove(obj)
-        return obj
+        return jsonify(obj.userToJson())
+    except ModuleNotFoundError:
+        abort((http.HTTPStatus.NO_CONTENT))
     except Exception:
         abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@user_controller.route('/usuario/remove', methods=["DELETE"])
+def deleteobj():
+    if request.get_json() is None:
+        abort(http.HTTPStatus.BAD_REQUEST)
+    try:
+        body = request.get_json()
+        obj = Users()
+        obj.dictToUser(body)
+        obj = service.findById(obj.id)
+        if(obj.id is None):
+            raise ModuleNotFoundError('Não encontrado')
+        service.remove(obj)
+        return jsonify(obj.userToJson())
+    except ModuleNotFoundError:
+        abort((http.HTTPStatus.NO_CONTENT))
+    except Exception:
+        abort(http.HTTPStatus.BAD_REQUEST)
+
+@user_controller.route('/usuario/administrador')
+def listaradmin():
+    try:
+        objs = service.listaradmin()
+        return jsonify(objs)
+    except Exception:
+        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+
+@user_controller.route('/usuario/administrador/<ident>')
+def listaradminid(ident):
+    try:
+        ident = int(ident)
+    except ValueError:
+        abort(http.HTTPStatus.BAD_REQUEST)
+
+    try:
+        objs = service.listaradminid(ident)
+        return jsonify(objs)
+    except Exception:
+        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+
+@user_controller.route('/usuario')
+def listarusuarios():
+    try:
+        objs = service.listarusuarios()
+        return jsonify(objs)
+    except Exception:
+        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+
+
+@user_controller.route('/usuario/<ident>')
+def listarusuarioid(ident):
+    try:
+        ident = int(ident)
+    except ValueError:
+        abort(http.HTTPStatus.BAD_REQUEST)
+
+    try:
+        objs = service.listarusuarioid(ident)
+        return jsonify(objs)
+    except Exception:
+        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+        
+@user_controller.route('/usuario/administrador', methods=['POST'])
+def inseriradmin():
+    if request.get_json() is None:
+        abort(http.HTTPStatus.BAD_REQUEST)
+    try:
+        body = request.get_json()
+        obj = Users()
+        obj.dictToUser(body)
+        service.save(obj)
+        return jsonify(obj.userToJson())
+    except Exception:
+        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+
+@user_controller.route('/usuario/administrador', methods=["POST"])
+def inserir():
+    if request.get_json() is None:
+        abort(http.HTTPStatus.BAD_REQUEST)
+    try:
+        body = request.get_json()
+        obj = Users()
+        obj.dictToUser(body)
+        service.save(obj)
+        return jsonify(obj.userToJson())
+    except Exception:
+        abort(http.HTTPStatus.INTERNAL_SERVER_ERROR)
+
+@user_controller.route('/usuario', methods=["POST"])
+def inserirusuario():
+    if request.get_json() is None:
+        abort(http.HTTPStatus.BAD_REQUEST)
+    try:
+        body = request.get_json()
+        obj = Users()
+        obj.dictToUser(body)
+        service.inserirusuario(obj)
+        if(obj.id is None):
+            raise Exception('Não autorizado')
+        return jsonify(obj.userToJson())
+    except Exception:
+        abort(http.HTTPStatus.BAD_REQUEST)
+
+@user_controller.route('/usuario/encontrar/<username>')
+def encontrarpornome(username):
+    try:
+        if(username is ''):
+            raise Exception('entrada não é válida')
+        obj = service.findByUsername(username)
+        if(obj is None):
+            raise ModuleNotFoundError('não encontrado.')
+        return jsonify(obj)
+    except ModuleNotFoundError:
+        abort(http.HTTPStatus.NO_CONTENT)
+    except Exception:
+        abort(http.HTTPStatus.BAD_REQUEST)
+
+@user_controller.route('/usuario/ordemAlfabetica')
+def listaremordemalfabetica():
+    try:
+        users = service.listAll()
+        usorted = sorted(users, key=lambda u: u.username)
+        retjson = []
+        for u in usorted:
+            #TODO filtrar users para não usuario logado não admin
+            retjson.append(u.userToJson())
+        return jsonify(retjson)
+    except Exception:
+        abort(http.HTTPStatus.BAD_REQUEST)
--- src/service/UserService.py
+++ src/service/UserService.py
@@ -6,35 +6,86 @@ class UserService:
     def listAll(self):
         return Users.query.all()
 
-    def findById(self, id):
-        return Users.query.filter_by(id=id).first()
+    def findById(self, ident):
+        return Users.query.filter_by(id=ident).first()
 
     def remove(self, obj):
         try:
-            ret = db_session.delete(obj)
+            db_session.delete(obj)
             db_session.commit()
-            return ret
+            return obj
         except Exception:
             db_session.rollback()
             return 'fail'
 
-    def save(self, obj):
-        try:
-            u = db_session.add(obj)
-            db_session.commit()
-            return u
-        except Exception:
-            db_session.rollback()
-            return 'fail'
 
     def update(self, obj):
         try:
-            u = db_session.merge(obj)
+            db_session.merge(obj)
             db_session.commit()
-            return u
+            return obj
         except Exception:
             db_session.rollback()
             return 'fail'
 
     def findByUsername(self, username):
-        return Users.query.filter_by(username=username).first()
+        users = Users.query.filter(Users.username.like("%"+username+"%")).all()
+        usersretorno = []
+        for user in users:
+            usersretorno.append(user.userToJson())
+        return usersretorno
+
+
+    def listaradmin(self):
+        try:
+            adminsResp = []
+            admins = Users.query.filter_by(authorities_id=1)
+            for admin in admins:
+                adminsResp.append(admin.userToJson())
+            return adminsResp
+        except Exception:
+            return 'fail'
+
+    def listaradminid(self, ident):
+        try:
+            admin = Users.query.filter_by(authorities_id=1, id=ident).first()
+            return admin.userToJson()
+        except Exception:
+            return 'fail'
+
+    def listarusuarios(self):
+        try:
+            usuariosResp = []
+            usuarios = Users.query.filter_by(authorities_id=2)
+            for usuario in usuarios:
+                usuariosResp.append(usuario.userToJson())
+            return usuariosResp
+        except Exception:
+            return 'fail'
+
+    def listarusuarioid(self, ident):
+        try:
+            usuario = Users.query.filter_by(authorities_id=2, id=ident).first()
+            return usuario.userToJson()
+        except Exception:
+            return 'fail'
+
+    def inserirusuario(self, obj):
+        try:
+            if(obj.authorities_id != 1):
+                db_session.add(obj)
+                db_session.commit()
+                return obj
+            raise Exception('Não permitido')
+        except Exception:
+            db_session.rollback()
+            return
+
+    def save(self, obj):
+        try:
+            db_session.add(obj)
+            db_session.commit()
+            return obj
+        except Exception:
+            db_session.rollback()
+            return 'fail'
